70cda69911be71298b920b9a59eb0dff1c89883b
Kiwiline0406 <angelinetavares.rp@gmail.com>
2025-07-25 14:15:59 +0200

    MAJ projet

--- pages/1_Projet.py
+++ pages/1_Projet.py
@@ -96,19 +96,19 @@ with tab4:
 
     with col1:
         st.image("images/illu_ange2.png", width=300)
-        st.write("<b>Angéline</b>")
+        st.write("Angéline")
         st.write("Méticuleuse et jusqu'au boutiste, elle n'hésitera pas à creuser le sujet jusqu'à la moëlle... Mais seulement si la rigolade est de mise.")
         # Ajouter description rapide
 
     with col2:
         st.image("images/illu_aurelie.png", width=300)
-        st.write("<b>Aurélie</b>")
+        st.write("Aurélie")
         st.write("Dynamique, elle voit la vie comme une aventure : faut que ça bouge ! Toujours prête à donner de son temps pour les autres, et relever de nouveaux défis !")
         # Ajouter description rapide    
 
     with col3:
         st.image("images/illu_yanis.png", width=300)
-        st.write("<b>Yanis</b>")
+        st.write("Yanis")
         st.write("Efficace, Yanis met les mains au clavier plus vite que son ombre. Complètement geek, il peut tenir n'importe quelle conversation, surtout s'il s'agit d'IT ou d'e-sport.")
         # Ajouter description rapide    
 
